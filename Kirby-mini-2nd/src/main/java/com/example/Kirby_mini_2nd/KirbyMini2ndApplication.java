3ef9818e20460454f3c03f59166f9a83498d672c
mysk423 <80307373+mysk423@users.noreply.github.com>
2024-10-28 18:04:54 +0900

    10-28
    프로젝트 생성

---
+++ Kirby-mini-2nd/src/main/java/com/example/Kirby_mini_2nd/KirbyMini2ndApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kirby_mini_2nd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirbyMini2ndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirbyMini2ndApplication.class, args);
+	}
+
+}
---
+++ Kirby-mini-2nd/src/test/java/com/example/Kirby_mini_2nd/KirbyMini2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kirby_mini_2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirbyMini2ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
