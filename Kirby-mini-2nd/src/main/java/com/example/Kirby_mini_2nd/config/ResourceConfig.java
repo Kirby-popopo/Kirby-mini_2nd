13c09ec829d8782b871555c65d9d75d108030ea1
mysk423 <80307373+mysk423@users.noreply.github.com>
2024-11-11 14:31:09 +0900

    11-11 파일 업로드 (이미지 관련)
    - 이미지에 대한 저장관련 메서드 정의, util로 뽑아놓았음.
    - multipart 설정 추가
    - 정적 리소스 경로 설정으로 캐싱된 데이터를 사용하지 않게 설정함

---
+++ Kirby-mini-2nd/src/main/java/com/example/Kirby_mini_2nd/config/ResourceConfig.java
@@ -0,0 +1,17 @@
+package com.example.Kirby_mini_2nd.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        // 정적 리소스의 경로를 직접 지정해주어서 캐싱된 것을 사용하지 않는다.
+        // 이미지 업로드 이후 서버에 요청시 못 찾던 문제 해결
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:src/main/resources/static/");
+    }
+
+}
---
+++ Kirby-mini-2nd/src/main/java/com/example/Kirby_mini_2nd/util/FileUtil.java
@@ -0,0 +1,27 @@
+package com.example.Kirby_mini_2nd.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
+
+public class FileUtil {
+    private static final String UPLOAD_DIR = "C:/MiniProject/Kirby-mini_2nd/Kirby-mini-2nd/src/main/resources/static/images/";
+    private static final String IMAGE_PATH = "http://localhost:8090/images/";
+
+    // 파일 저장  메서드
+    public static String SaveFile(MultipartFile file) {
+        try {
+            String fileName = UUID.randomUUID().toString() + file.getOriginalFilename();
+            File destination = new File(UPLOAD_DIR + fileName);
+            file.transferTo(destination);
+            
+            // 파일 경로와 이름
+            // 저장시 url 만들어주기.
+            return IMAGE_PATH + fileName;
+        } catch (Exception e) {
+            return ("에러발생 : " + e.getMessage());
+        }
+
+    }
+}
