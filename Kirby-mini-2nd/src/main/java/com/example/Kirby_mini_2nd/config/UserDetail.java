744f500ea6848c143db6e1b80e4a08a7b5c9883d
yoojihun <dbwlgns1204@gmail.com>
2024-11-18 14:10:54 +0900

    2024-11-18 security 적용할때 필요한 Userdetail

---
+++ Kirby-mini-2nd/src/main/java/com/example/Kirby_mini_2nd/config/UserDetail.java
@@ -0,0 +1,52 @@
+package com.example.Kirby_mini_2nd.config;
+
+import com.example.Kirby_mini_2nd.repository.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetail implements UserDetails {
+
+    private User user;
+
+    public UserDetail(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getUserPw();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+}
