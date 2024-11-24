0f55c434f27ec45d1e8092e5813748a7ab8ba508
Jioni <98302106+JionisGenius@users.noreply.github.com>
2024-11-24 15:02:57 +0900

    11 - 24 지원이 좋아요 기능 완성 내용 커밋

--- Kirby-mini-2nd/src/main/java/com/example/Kirby_mini_2nd/controller/LikeCtrl.java
+++ Kirby-mini-2nd/src/main/java/com/example/Kirby_mini_2nd/controller/LikeCtrl.java
@@ -3,21 +3,30 @@ package com.example.Kirby_mini_2nd.controller;
 import com.example.Kirby_mini_2nd.model.ResponseModel;
 import com.example.Kirby_mini_2nd.repository.entity.Likes;
 import com.example.Kirby_mini_2nd.repository.repo.LikesRepo;
+import com.example.Kirby_mini_2nd.service.LikeSvc;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class LikeCtrl {
-    LikesRepo likesRepo;
-    public LikeCtrl(Likes likes) {
-        this.likesRepo = likesRepo;
+    LikeSvc likeSvc;
+    public LikeCtrl(LikeSvc likeSvc) {
+        this.likeSvc = likeSvc;
     }
-    //@GetMapping("/like")
-    //public ResponseEntity<ResponseModel>readLike(@)
+    @GetMapping("/like/{postPk}")
+    public ResponseEntity<ResponseModel>readLike(@PathVariable int PostPk){
+        List<Likes> readLikes = likeSvc.searchPostLike(PostPk);
+        return ResponseModel.MakeResponse(readLikes, HttpStatus.OK);
+    }
+    @PostMapping("/like")
+    public ResponseEntity<ResponseModel>likeClick(@RequestParam int PostPk,@RequestParam String userId){
+        String saveLikes= likeSvc.addLike(PostPk,userId);
+        return ResponseModel.MakeResponse(saveLikes,HttpStatus.OK);
+    }
 
 
 
