8c9d3cab9ad485e07afcce333b8d1a23a83ca5cf
mysk423 <80307373+mysk423@users.noreply.github.com>
2024-11-24 14:19:23 +0900

    11-24 컨트롤러 수정내용

--- Kirby-mini-2nd/src/main/java/com/example/Kirby_mini_2nd/controller/CommnetCtrl.java
+++ Kirby-mini-2nd/src/main/java/com/example/Kirby_mini_2nd/controller/CommnetCtrl.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class CommnetCtrl {
     CommentSvc commentSvc;
@@ -23,27 +23,28 @@ public class CommnetCtrl {
     }
 
     @PostMapping("/comment")
-    public ResponseEntity<ResponseModel> SaveComment(@RequestParam("content")Comments content) {
+    public ResponseEntity<ResponseModel> SaveComment(@RequestBody Comments content) {
             String savedComment = commentSvc.SaveComment(content);
             return ResponseModel.MakeResponse(savedComment, HttpStatus.OK);
     }
-    @GetMapping("/comment")
-    // 이따가 수정해줄게
-    public ResponseEntity<ResponseModel>ReadComment(@RequestBody Map<String,Integer> requestData){
-        int Pk = requestData.get("postPk");
-        List<Comments> ListComments =commentSvc.ReadComment(Pk);
+
+    @GetMapping("/comment/{postPk}")
+    public ResponseEntity<ResponseModel>ReadComment(@PathVariable String postPk){
+        int pk = Integer.parseInt(postPk);
+        List<Comments> ListComments =commentSvc.ReadComment(pk);
         return ResponseModel.MakeResponse(ListComments,HttpStatus.OK);
     }
+
     @PostMapping("/updateComment")
     public ResponseEntity<ResponseModel> UpdateComment(@RequestParam("CommentPk")int commentPk,
                                                        @RequestParam ("Content")String content){
         String updated=commentSvc.UpdateComment(commentPk,content);
         return ResponseModel.MakeResponse(updated,HttpStatus.OK);
     }
+
     @PostMapping("/deleteComment")
     public ResponseEntity<ResponseModel> DeleteComment(@RequestParam("CommentPk")int commentPk){
         String deleted= commentSvc.DeleteComment(commentPk);
         return ResponseModel.MakeResponse(deleted,HttpStatus.OK);
     }
-
 }
